1bb236bab8ce8593bceaeadfe3398b95fb24e84c
Alec <alexander.pureza@bitsfabrik.com>
2023-09-08 12:30:49 +0200

    ✨ reordering queues to updatepriority

--- RayWenderlichLibrary/RayWenderlichLibrary/Controllers/QueuedTutorialController.swift
+++ RayWenderlichLibrary/RayWenderlichLibrary/Controllers/QueuedTutorialController.swift
@@ -52,11 +52,13 @@ class QueuedTutorialController: UIViewController {
     }()
     
     private lazy var deleteButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.trash, target: self, action: #selector(deleteSelectedItems))
-    private lazy var updateButton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain,target: self, action: #selector(deleteButtonClicked))
-    private lazy var applyUpdatesButton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "play.fill"), style: .plain, target: self, action: #selector(deleteButtonClicked))
+    private lazy var updateButton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain,target: self, action: #selector(triggerUpdates))
+    private lazy var applyUpdatesButton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "play.fill"), style: .plain, target: self, action: #selector(applyUpdates))
     
     private var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     private lazy var dataSource: UICollectionViewDiffableDataSource<QueuedSection, Tutorial> = UICollectionViewDiffableDataSource(collectionView: self.collectionView, cellProvider: {_,_,_  in return nil })
+    
+    private static let badgeElementKind = "badge-element-kind"
     
     // /////////////////////////////////////////////////////////////////////////
     // MARK: - QueuedTutorialController
@@ -77,6 +79,7 @@ class QueuedTutorialController: UIViewController {
         
         self.collectionView.collectionViewLayout = self.configureCollectionViewLayout()
         self.collectionView.register(QueueCell.self, forCellWithReuseIdentifier: QueueCell.reuseIdentifier)
+        self.collectionView.register(BadgeSupplementaryView.self, forSupplementaryViewOfKind: QueuedTutorialController.badgeElementKind, withReuseIdentifier: BadgeSupplementaryView.reuseIdentifier)
         self.collectionView.backgroundColor = .lightGray.withAlphaComponent(0.1)
         self.configureDataSource()
         
@@ -119,13 +122,64 @@ class QueuedTutorialController: UIViewController {
         self.isEditing.toggle()
     }
     
+    // for this tutorial just manually updating to show the action
+    @objc
+    func triggerUpdates() {
+        let indexPaths = self.collectionView.indexPathsForVisibleItems
+        let randomIndexPath = indexPaths[Int.random(in: 0..<indexPaths.count)]
+        let tutorial = self.dataSource.itemIdentifier(for: randomIndexPath)
+        tutorial?.updateCount = 3
+        
+        // show badge
+        let badgeView = self.collectionView.supplementaryView(forElementKind: QueuedTutorialController.badgeElementKind, at: randomIndexPath)
+        badgeView?.isHidden = false
+    }
+    
+    @objc
+    func applyUpdates() {
+        
+        let tutorials = self.dataSource.snapshot().itemIdentifiers
+        
+        // at least 1 tutorial and first for comparison
+        if var firstTutorial = tutorials.first, tutorials.count > 2 {
+            // filter out the tutorials with updated
+            let tutorialsWithUpdates = tutorials.filter( { $0.updateCount > 0 } )
+            
+            var currentSnapshot = self.dataSource.snapshot()
+            
+            tutorialsWithUpdates.forEach { tutorial in
+                if tutorial != firstTutorial {
+                    currentSnapshot.moveItem(tutorial, beforeItem: firstTutorial)
+                    firstTutorial = tutorial
+                    tutorial.updateCount = 0
+                }
+                
+                if let indexPath = self.dataSource.indexPath(for: tutorial) {
+                    let badgeView = self.collectionView.supplementaryView(forElementKind: QueuedTutorialController.badgeElementKind, at: indexPath)
+                    
+                    badgeView?.isHidden = true
+                }
+            }
+            
+            self.dataSource.apply(currentSnapshot, animatingDifferences: true)
+        }
+    }
+    
     // /////////////////////////////////////////////////////////////////////////
     // MARK: - CollectionView Configuration
     
     func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
         
+        // Ask how and if you can make that with snapkit
+        let anchorEdges: NSDirectionalRectEdge = [.top, .trailing]
+        let offset = CGPoint(x: 0.3, y: -0.3)
+        let badgeAnchor = NSCollectionLayoutAnchor(edges: anchorEdges, fractionalOffset: offset)
+        let badgeSize = NSCollectionLayoutSize(widthDimension: .absolute(20), heightDimension: .absolute(20))
+        
+        let badge = NSCollectionLayoutSupplementaryItem(layoutSize: badgeSize, elementKind: QueuedTutorialController.badgeElementKind, containerAnchor: badgeAnchor)
+        
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize, supplementaryItems: [badge])
         item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
         
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(120))
@@ -137,7 +191,7 @@ class QueuedTutorialController: UIViewController {
     }
     
     func configureDataSource() {
-        self.dataSource = UICollectionViewDiffableDataSource<QueuedSection, Tutorial>(collectionView: self.collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, tutorial: Tutorial) in 
+        self.dataSource = UICollectionViewDiffableDataSource<QueuedSection, Tutorial>(collectionView: self.collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, tutorial: Tutorial) in
             
             guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: QueueCell.reuseIdentifier, for: indexPath) as? QueueCell else {
                 return nil
@@ -149,6 +203,22 @@ class QueuedTutorialController: UIViewController {
             cell.publishDateLabel.text = tutorial.formattedDate(using: self.dateFormatter)
             
             return cell
+        }
+        
+        // setting badge if element has an update
+        
+        self.dataSource.supplementaryViewProvider = { [weak self] (collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? in
+            
+            guard let self = self, let tutorial = self.dataSource.itemIdentifier(for: indexPath), let badgeView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: BadgeSupplementaryView.reuseIdentifier, for: indexPath) as? BadgeSupplementaryView else { return nil }
+            
+            // check update
+            if tutorial.updateCount > 0 {
+                badgeView.isHidden = false
+            } else {
+                badgeView.isHidden = true
+            }
+            
+            return badgeView
         }
     }
     
@@ -184,17 +254,9 @@ extension QueuedTutorialController {
         self.collectionView.indexPathsForVisibleItems.forEach { indexPath in
             guard let cell = self.collectionView.cellForItem(at: indexPath) as? QueueCell else { return }
             cell.isEditing = self.isEditing
-            
             if !self.isEditing {
                 cell.isSelected = false
             }
         }
-    }
-    
-    @IBAction func triggerUpdates() {
-        
-    }
-    
-    @IBAction func applyUpdates() {
     }
 }
---
+++ RayWenderlichLibrary/RayWenderlichLibrary/Views/BadgeSupplementaryView.swift
@@ -0,0 +1,44 @@
+//
+//  BadgeSupplementaryView.swift
+//  RayWenderlichLibrary
+//
+//  Created by Alec on 08.09.23.
+//  Copyright © 2023 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+// /////////////////////////////////////////////////////////////////////////
+// MARK: - BadgeSupplementaryView -
+// /////////////////////////////////////////////////////////////////////////
+
+final class BadgeSupplementaryView: UICollectionReusableView {
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Properties
+    
+    static let reuseIdentifier = String(describing:  BadgeSupplementaryView.self)
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Functions
+    
+    private func configure() {
+        self.backgroundColor = UIColor(named: "rw-green")
+        let radius = self.bounds.width / 2.0
+        self.layer.cornerRadius = radius
+    }
+}
+
